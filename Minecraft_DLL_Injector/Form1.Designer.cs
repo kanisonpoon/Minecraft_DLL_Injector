2923b683f374ccb0c522e740d7d363d86cd38503
EchoHackCmd <EchoHackCmd@mail.com>
2020-07-13 07:51:48 +0100

    Clean Upload

---
+++ Minecraft_DLL_Injector/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Minecraft_DLL_Injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(12, 10);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(205, 13);
+            this.infoLabel.TabIndex = 0;
+            this.infoLabel.Text = "Minecraft DLL Injector, By EchoHackCmd";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Specify DLL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.infoLabel);
+            this.Name = "Form1";
+            this.Text = "Minecraft DLL Injector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
